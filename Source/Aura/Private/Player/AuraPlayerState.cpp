57e3cda5923f4390b26b927bf24ae635b60c562c
Afonso Pordeus <afonsopordeus@gmail.com>
2026-01-15 00:01:27 -0300

    Constructing AbilitySystemComponent and AttributeSets

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -2,8 +2,20 @@
 
 
 #include "Player/AuraPlayerState.h"
+#include <AbilitySystem/AuraAbilitySystemComponent.h>
+#include <AbilitySystem/AuraAttributeSet.h>
 
 AAuraPlayerState::AAuraPlayerState()
 {
-	NetUpdateFrequency = 100.0f;
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+
+	SetNetUpdateFrequency(100.0f);
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
--- Source/Aura/Public/Characters/AuraCharacterBase.h
+++ Source/Aura/Public/Characters/AuraCharacterBase.h
@@ -4,19 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
 #include "AuraCharacterBase.generated.h"
 
+class UAbilitySystemComponent;
+class UAttributeSet;
+
 UCLASS(Abstract)
-class AURA_API AAuraCharacterBase : public ACharacter
+class AURA_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AAuraCharacterBase();
 
+	//* ~Begin IAbilitySystemInterface */
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	//* ~End IAbilitySystemInterface */
+
+	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
+
 protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditDefaultsOnly, Category="Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
